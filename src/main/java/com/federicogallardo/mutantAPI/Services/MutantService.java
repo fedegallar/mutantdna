d53d2e0854ef6642aec5c11d763ac4f8106a5cc4
Federico Gallardo <fedegallar2006@hotmail.com>
2019-11-24 22:27:11 -0300

    Eliminar personas y verificaciòn de existencias por DNI

--- src/main/java/com/federicogallardo/mutantAPI/Services/MutantService.java
+++ src/main/java/com/federicogallardo/mutantAPI/Services/MutantService.java
@@ -1,15 +1,14 @@
 package com.federicogallardo.mutantAPI.Services;
 
-import java.util.Collection;
 import java.util.List;
 
 import javax.transaction.Transactional;
 
 import com.federicogallardo.mutantAPI.DTOs.IStats;
 import com.federicogallardo.mutantAPI.DTOs.PersonDTO;
-import com.federicogallardo.mutantAPI.DTOs.StatsDTO;
 import com.federicogallardo.mutantAPI.Entities.Person;
 import com.federicogallardo.mutantAPI.Factories.MutantDNASearchStrategyFactory;
+import com.federicogallardo.mutantAPI.MutantExceptions.PersonExistsException;
 import com.federicogallardo.mutantAPI.Repositories.MutantRepository;
 import com.federicogallardo.mutantAPI.Strategies.MutantDNASearchStrategy;
 
@@ -35,9 +34,19 @@ public class MutantService{
     }
 
     @Transactional
-    public boolean verifyDNAandSave(final PersonDTO persondto) throws Exception{
+    public boolean CheckPersonDB(String dni) throws PersonExistsException{
+      if(this.repository.findPersonByDNI(dni).isEmpty())
+        return false;
+      else
+        throw new PersonExistsException();
+    }
+    @Transactional
+    public boolean verifyDNAandSave(final PersonDTO persondto) throws Exception, PersonExistsException{
         //REVISAR SI LA PERSONA YA EXISTE?
         try{
+            // LLAMA AQUI PARA VERIFICAR SI EXISTE ESA PERSONA CON DNI
+            this.CheckPersonDB(persondto.getDni());
+            
             boolean result = false;
             final MutantDNASearchStrategy busquedaHorizontal = MutantDNASearchStrategyFactory.create("Horizontal");
             final MutantDNASearchStrategy busquedaVertical = MutantDNASearchStrategyFactory.create("Vertical");
@@ -59,13 +68,31 @@ public class MutantService{
             else{
               person.setMutant(false);
             }
-            //repository.save(person);
+            repository.save(person);
             if(result == true){
                 return true;
             }
             return false;
-        }catch(final Exception e){
+        }
+        catch(final PersonExistsException e){
+          throw new PersonExistsException();
+        }
+        catch(final Exception e){
             throw new Exception();
         }
+    }
+
+    @Transactional
+    public String DeletePerson(int id) throws Exception, IllegalArgumentException{
+      try {
+        this.repository.deleteById(id);
+        return "Person deleted";
+      }
+      catch(IllegalArgumentException e){
+        throw new IllegalArgumentException();
+      } 
+      catch (Exception e) {
+        throw new Exception();
+      }
     }
 }
