9e92e61feb13ae3e48af0c944a5a8ee9d43eda07
Federico Gallardo <fedegallar2006@hotmail.com>
2019-11-24 22:24:10 -0300

    Paginacion de personas

--- src/main/java/com/federicogallardo/mutantAPI/Controllers/MutantPagingController.java
+++ src/main/java/com/federicogallardo/mutantAPI/Controllers/MutantPagingController.java
@@ -4,10 +4,12 @@ import javax.transaction.Transactional;
 
 import com.federicogallardo.mutantAPI.Entities.Person;
 import com.federicogallardo.mutantAPI.Repositories.MutantPagingRepository;
+import com.federicogallardo.mutantAPI.Services.MutantPagingService;
 
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,22 +17,23 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("api/v1/mutant")
 public class MutantPagingController {
 
-    MutantPagingRepository repository;
+    MutantPagingService service;
 
-    public MutantPagingController(MutantPagingRepository repository) {
+    public MutantPagingController(MutantPagingService service) {
         super();
-        this.repository = repository;
+        this.service = service;
     }
     
 
-    @GetMapping("/")
+    @GetMapping("/{page}")
     @Transactional
-    public Page<Person> getAllPeople(Pageable pageable){
+    public Page<Person> getAllPeople(Pageable pageable, @PathVariable int page){
         try{
-            return repository.findAllPeopleById(pageable);
+            return this.service.getPeople(pageable, page);
         }catch(Exception e){
             return Page.empty();
         }
     }
+
 
 }
